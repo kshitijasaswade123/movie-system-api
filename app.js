5b3f20b6163d853efff6fe0dfda451840d4e21ce
kshitijasaswade <61146709+kshitijasaswade123@users.noreply.github.com>
2024-02-09 13:48:25 +0530

    add bydefault route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res)=>{
+  res.send("Movie System")
+})
+
 
 app.use("/api/movie", movie);
 app.use("/api/categry", categry);
